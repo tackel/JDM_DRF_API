4adaa884221ef766b1f782d7c8c3d84124b2ed4a
Fernando Verri <74513344+tackel@users.noreply.github.com>
2022-10-07 15:33:21 -0300

    arreglo create funcion

--- apps/productos/api/viewset/product_viewset.py
+++ apps/productos/api/viewset/product_viewset.py
@@ -6,10 +6,16 @@ from apps.productos.api.serializers.product_serializer import ProductSerializer,
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
+
     def get_queryset(self, pk = None):
         if pk is None:
             return self.get_serializer().Meta.model.objects.filter(state=True)
-        return self.get_serializer().Meta().model.objects.filter(id=pk, state=True).first()            
+        return self.get_serializer().Meta.model.objects.filter(id=pk, state=True).first()
+
+    def get_queryset_false(self, pk = None):   
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state=False)
+        return self.get_serializer().Meta.model.objects.filter(id=pk, state=False).first()         
 
     def list(self, request):
         product_serializer = self.get_serializer(self.get_queryset(), many=True)
@@ -18,23 +24,33 @@ class ProductViewSet(viewsets.ModelViewSet):
             'rows': product_serializer.data
         }
         return Response(product_serializer.data, status=status.HTTP_200_OK)
-    
+
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
+        #print(request.data['codigo'])
         if serializer.is_valid():
             serializer.save()
             return Response({'message':'Producto creado correctamente'}, status=status.HTTP_201_CREATED)
+        # si el producto existe pero esta en estado false.
+        producto = self.get_queryset_false().filter(codigo=request.data['codigo']).first()
+        if producto:
+            producto.state = True
+            producto.save()
+            self.update(request, producto.id)
+            print(producto.id)
+            return Response({'message':f'producto {producto} activado y modificado'}, status=status.HTTP_201_CREATED)
         return Response({'message':'Error al ingresar los datos', 'error': serializer.errors}, status.HTTP_400_BAD_REQUEST)
 
-    def update(self, request, pk=None):
+    def update(self, request, pk=None, **kwargs):
         if self.get_queryset(pk):
             # send information to serializer referencing the instance
-            #data = validate_files(request.data, 'image', True)
-            product_serializer = self.serializer_class(self.get_queryset(pk), data=data)            
+            #data = self.validate_files(request.data, 'codigo', True) 
+            product_serializer = self.serializer_class(self.get_queryset(pk), data=request.data)            
             if product_serializer.is_valid():
                 product_serializer.save()
                 return Response({'message':'Producto actualizado correctamente!'}, status=status.HTTP_200_OK)
             return Response({'message':'Error al modificar los datos', 'error':product_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message':'Error, id ingresado no existe'}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
         # Eliminacon logica, el producto sigue existiendo pero se cambia el estado
--- apps/productos/models.py
+++ apps/productos/models.py
@@ -8,20 +8,15 @@ class Product(models.Model):
 
     # TODO: Define fields here
     id = models.AutoField(primary_key=True)
-    codigo = models.CharField(
-        'Codio de Barras', max_length=50, null=False, blank=False, unique=True)
-    nombre = models.CharField(
-        'Nombre de Producto', max_length=150, unique=True, blank=False, null=False)
+    codigo = models.CharField('Codio de Barras', max_length=50, null=False, blank=False, unique=True)
+    nombre = models.CharField('Nombre de Producto', max_length=150, blank=False, null=False)
     costo = models.IntegerField('Costo de compra')
     precio = models.IntegerField('Precio de venta')
     #description = models.TextField('Descripción de Producto', blank=False, null=False)
     #category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Categoria de Producto', null=True)
-    created_date = models.DateField(
-        'Fecha de Creación', auto_now=False, auto_now_add=True)
-    modified_date = models.DateField(
-        'Fecha de Modificación', auto_now=True, auto_now_add=False)
-    deleted_date = models.DateField(
-        'Fecha de Eliminación', auto_now=True, auto_now_add=False)
+    created_date = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+    modified_date = models.DateField('Fecha de Modificación', auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField('Fecha de Eliminación', auto_now=True, auto_now_add=False)
     state = models.BooleanField('Estado', default=True)
 
     class Meta:
@@ -32,4 +27,4 @@ class Product(models.Model):
 
     def __str__(self):
         """Unicode representation of Product."""
-        return self.nombre
+        return f'{self.codigo} {self.nombre}'
